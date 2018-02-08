3750ee960f89531f642834f58ff91a1f5f9d1994
gaowei <gaowei@tidebuy.net>
2018-02-08 16:38:02 +0800

    add codemirror

--- src/routes/Blog/AllBlog.js
+++ src/routes/Blog/AllBlog.js
@@ -2,11 +2,21 @@
 import { Component } from 'react';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 
+const initialSource = `
+# Live demo
+Changes are automatically rendered as you type.
+* Implements [GitHub Flavored Markdown](https://github.github.com/gfm/)
+* Renders actual, "native" React DOM elements
+* Allows you to escape or skip HTML (try toggling the checkboxes above)
+* If you escape or skip the HTML, no \`dangerouslySetInnerHTML\` is used! Yay!
+## HTML block below
+`
 export default class AllBlog extends Component {
   render() {
     return (
       <PageHeaderLayout >
-        所有博客
+        <div>
+        </div>
       </PageHeaderLayout>);
   }
 }
--- src/routes/Blog/BlogCategory.js
+++ src/routes/Blog/BlogCategory.js
@@ -1,25 +1,87 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { Component } from 'react';
 import { connect } from 'dva';
-import { Table, Divider } from 'antd';
+import { Table, Divider, Form, Modal, Input, message, Button } from 'antd';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 import styles from './BlogCategory.less';
 
+const FormItem = Form.Item;
 
-@connect(({ blog, loading }) => {
-  console.log(loading);
+const CreateForm = Form.create()((props) => {
+  const { modalVisible, form, handleOK, handleModalVisible, type, record } = props;
+  const okHandle = () => {
+    form.validateFields((err, fieldsValue) => {
+      if (err) return;
+      form.resetFields();
+      handleOK(fieldsValue);
+    });
+  };
+  const CancelHandle = () => {
+    form.resetFields();
+    handleModalVisible();
+  };
+  return (
+    <Modal
+      title={type == 1 ? '新建' : '修改'}
+      visible={modalVisible}
+      onOk={okHandle}
+      onCancel={CancelHandle}
+    >
+      <FormItem
+        labelCol={{ span: 5 }}
+        wrapperCol={{ span: 15 }}
+        label="分类名称"
+      >
+        {form.getFieldDecorator('bCategory_name', {
+          initialValue: record ? record.bCategory_name : null,
+          rules: [{ required: true, message: '请输入分类名称' }],
+        })(
+          <Input placeholder="请输入分类名称" />
+        )}
+      </FormItem>
+      <FormItem
+        labelCol={{ span: 5 }}
+        wrapperCol={{ span: 15 }}
+        label="路径"
+      >
+        {form.getFieldDecorator('bCategory_path', {
+          initialValue: record ? record.bCategory_path : null,
+          rules: [{ required: true, message: '请输入路径' }],
+        })(
+          <Input placeholder="请输入路径" />
+        )}
+      </FormItem>
+      <FormItem
+        labelCol={{ span: 5 }}
+        wrapperCol={{ span: 15 }}
+        label="Sort"
+      >
+        {form.getFieldDecorator('bCategory_sort', {
+          initialValue: record ? record.bCategory_sort : 1000,
+          rules: [{ required: true, message: 'Please input some...' }],
+        })(
+          <Input type="number" />
+        )}
+      </FormItem>
+    </Modal>
+  );
+});
+
+@connect(({ blog }) => {
   return (
     {
       blogCategorys: blog.blogCategorys,
-      loading: loading && loading.models.list,
     }
   );
 })
 export default class BlogCategory extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      modalVisible: false,
+    };
     this.columns = [{
-      title: '分类',
+      title: '分类名称',
       dataIndex: 'bCategory_name',
       key: 'bCategory_name',
     }, {
@@ -36,6 +98,8 @@ export default class BlogCategory extends Component {
       key: 'operation',
       render: this._operationAction,
     }];
+    this.type = 1;
+    this.record = null;
   }
   componentDidMount() {
     this.props.dispatch({
@@ -45,7 +109,7 @@ export default class BlogCategory extends Component {
   _operationAction=(text, record) => {
     return (
       <span>
-        <a href="#">修改</a>
+        <a onClick={() => { this.type = 2, this.record = record, this.handleModalVisible(true); }}>修改</a>
         <Divider type="vertical" />
         <a href="#">禁用</a>
         <Divider type="vertical" />
@@ -53,12 +117,39 @@ export default class BlogCategory extends Component {
       </span>
     );
   }
+  handleModalVisible = (flag) => {
+    this.setState({
+      modalVisible: !!flag,
+    });
+  }
+  handleOK = (fields) => {
+    console.log(fields)
+    message.success('添加成功');
+    this.setState({
+      modalVisible: false,
+    });
+  }
   render() {
+    const parentMethods = {
+      handleOK: this.handleOK,
+      handleModalVisible: this.handleModalVisible,
+      type: this.type,
+      record: this.record,
+    };
     return (
       <PageHeaderLayout >
         <div className={styles.standardList} >
-          <Table loading={this.props.loading} columns={this.columns} pagination={false} dataSource={this.props.blogCategorys.data} bordered />
+          <div className={styles.tableListOperator}>
+            <Button icon="plus" type="primary" onClick={() => { this.type = 1; this.record = null; this.handleModalVisible(true); }}>
+              新建
+            </Button>
+          </div>
+          <Table rowKey="bCategory_id" columns={this.columns} pagination={false} dataSource={this.props.blogCategorys.data} bordered />
         </div>
+        <CreateForm
+          {...parentMethods}
+          modalVisible={this.state.modalVisible}
+        />
       </PageHeaderLayout>);
   }
 }
