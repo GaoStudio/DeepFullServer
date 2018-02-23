5ce1f9f435505be6d59d05222ce276533c77c636
Jaye Gao <810915983@qq.com>
2018-02-23 22:26:27 +0800

    add allblog

--- src/routes/Blog/AllBlog.js
+++ src/routes/Blog/AllBlog.js
@@ -1,12 +1,34 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { Component } from 'react';
+import { Component,Table } from 'react';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
-
+import StandardTable from '../../components/StandardTable';
+const data = [{
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+}, {
+    key: '2',
+    name: 'Joe Black',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+}, {
+    key: '3',
+    name: 'Jim Green',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+}, {
+    key: '4',
+    name: 'Jim Red',
+    age: 32,
+    address: 'London No. 2 Lake Park',
+}];
 
 export default class AllBlog extends Component {
   render() {
     return (
       <PageHeaderLayout >
+          <Table data={data}/>
       </PageHeaderLayout>
     );
   }
